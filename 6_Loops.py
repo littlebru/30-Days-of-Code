8c3d092e1f222d16c3385b5c4b4f6ecc4f666c89
Bruna Gomes <brunaclegomes@hotmail.com>
2020-07-23 17:01:52 -0300

    initial commit

---
+++ 6_Loops.py
@@ -0,0 +1,14 @@
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input())
+
+    for i in range(1,11):
+        mult = '{0} x {1} = {2}'
+        print(mult.format(n, i, n*i))
